2671aae5b324695849cc55917059b11cdfdfe8b1
LeeWonkyung <leewk92@estsoft.com>
2016-01-18 16:34:06 +0900

    gtest setting completed

--- Matrix/Matrix/main.cpp
+++ Matrix/Matrix/main.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
-#include <gtest/gtest.h>
+#include <gtest\gtest.h>
 
+int add(int a, int b){
+	return a+b;
+}
+
+TEST(ADD_TEST,ADD_1_1){
+	EXPECT_EQ(2,add(1,1));
+}
+
+int main(int argc, char* argv[]){
+	testing::InitGoogleTest(&argc,argv);
+	RUN_ALL_TESTS();
+
+	return 0;
+}
