cb6b84c704634f6b5c333223fec0a89fc8d5a710
LeeWonkyung <leewk92@estsoft.com>
2016-01-26 13:05:27 +0900

    GenericMatrixOperation Class is created

---
+++ Matrix/Matrix/GenericMatrixOperation.h
@@ -0,0 +1,16 @@
+#ifndef GENERICMATRIXOPERATION_H
+#define GENERICMATRIXOPERATION_H
+
+#include <iostream>
+using namespace std;
+
+class GenericMatrixOperation{
+private:
+
+public:
+	GenericMatrixOperation();
+
+
+};
+
+#endif
